b6bbceb9ee499ec94379b721dcebd5551481417f
le huu phi <lehuuphi0@gmail.com>
2022-08-07 16:57:38 +0700

    nopbt

---
+++ lab10/scrip.js
@@ -0,0 +1,113 @@
+//alert("helooo1233");    
+/*let myname ="huuphi";
+let myage ="22"
+let myjob = "studen"
+const bithday ="03-07-2001"
+
+
+console.log(myname);
+console.log(myname,myjob,bithday)
+myname = "hehe";
+console.log(myname);
+
+let numBer = 123;
+let float = 123.213;
+let saperator = 100_000_000;
+console.log(numBer);
+console.log(saperator);
+console.log(typeof numBer);
+
+let age = "30"
+let firstName = "phi"
+let lastName = "lê"
+let message = "heloo, i\' "
+console.log(firstName);
+console.log(lastName);
+console.log(message);
+console.log(lastName+firstName);
+//
+let mesage =(" heloo everyday," + firstName + lastName );
+console.log(mesage); 
+let isOn = true;
+let isHandsome = false;
+let isTall = false; 
+let mycar = null;
+let mylove;
+// object là kiểu phức hợp cho phép lưu trứ nhiều giá trị trong cùng 1 biến duy nhất 
+// mỗi giá trị có định danh riêng gọi là key 
+// cú pháp
+const phi{
+      firstname:"phi",
+      lastname: "le"
+}
+ 
+const he{
+    firstname:"fefe",
+    lastname:"fefe"
+}
+// mảng cho phép mình lưu trứ 1 danh sách giá trị 
+let students[phi,he]
+
+
+function helloo(who){
+    console.log("hello" + who)
+}
+** gán giá trị 
+ 
+const.gretting = function(who){
+    console.log("heloo" + who);
+}
+// để xử dụng đoạn mã được đóng gói trong hàm 
+//tên hàm
+// hàm cso tể nhận đối số 
+helloo("phi");
+
+let age = 30;
+const sum = function(a,b){
+    console.log(age);
+    const resulf = a + b;
+    return resulf;
+};
+const sumOf1and2 = sum(1,2);
+console.log(sumOf1and2);
+
+
+function sum(a,b){
+    const resulf = a + b;
+    return resulf
+}
+const sumOf1and2 = sum(2,5)
+console.log(sumOf1and2);
+
+function sub(a,b){
+    const resulf = a - b;
+    return resulf
+}
+const subOf1and2 = sub(6,5)
+console.log(subOf1and2);
+
+function nul(a,b){
+    const resulf = a * b;
+    return resulf
+}
+const nulOf1and2 = nul(6,5)
+console.log(nulOf1and2);
+
+function div(a,b){
+    const resulf = a/b;
+    return resulf
+}
+const divOf1and2 = div(30,5)
+console.log(divOf1and2);
+
+let currentyear = 2022
+function findyearold(yearOfBirth){}
+ return currentyear - findyearold ;
+console.log(findyearold(2001));
+*/
+function writelog(mesage){
+    console.log(mesage)
+}
+writelog('123e')
+
+
